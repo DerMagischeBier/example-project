5a32a515139b7b9dddbf393995538e0d045138d8
DerMagischeBier <some_magic@bier.de>
2022-12-08 21:45:04 +0100

    Just add an additional function

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@ fn add_numbers(a: u16, b: u16, c: u16) -> u16 {
     a + b + c
 }
 
+fn add_more_numbers(a: u16, b: u16, c: u16, d: u16) -> u16 {
+    a + b + c + d
+}
+
 fn main() {
     let a = 3;
     let b = 6;
